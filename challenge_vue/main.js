4822600d25a750448a1b7a1e1c162833e42cc30b
兼城賢人 <>
2020-06-21 19:30:20 +0900

    vue　ドキュメント4 for文

--- challenge_vue/main.js
+++ challenge_vue/main.js
@@ -8,5 +8,20 @@ var app = new Vue({
     inStock: true,
     inventory: 2,
     // inventory: 2
+    details: ["80% cotton", "20% polyester", "gender-neutural"],
+    valiants: [
+      {
+      //↓vueが各ノードのIDを追跡できるように、特別なキー属性にすることを勧める
+        // id: 1,
+        // color: "red",
+        valiantId: 1,
+        valiantColor: "red",
+      },
+      {
+        id: 2,
+        color: "bule"
+      }
+    ],
+    sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
   }
 });
